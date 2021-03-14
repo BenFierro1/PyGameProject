9d1e1b43dcbb5e2b581d1890883a1bc53f505635
Benjamin Fierro <Ben_Fierro1@baylor.edu>
2021-03-13 22:02:45 -0600

    simplify run_game()

--- settings.py
+++ settings.py
@@ -8,5 +8,6 @@ class Settings:
         """Initialize the game's settings."""
         # Screen settings
         self.screen_width = 1200
-        self.screen_height = 800
+        ### I have to use screen height of 700 for my computer ###
+        self.screen_height = 700
         self.bg_color = (230, 230, 230)
