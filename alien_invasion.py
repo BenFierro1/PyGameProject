21697a86543e172d8b7d5ef76ecca7f63c874d0d
Benjamin Fierro <Ben_Fierro1@baylor.edu>
2021-03-14 18:06:03 -0500

    Ships remaining added

--- alien_invasion.py
+++ alien_invasion.py
@@ -71,6 +71,7 @@ class AlienInvasion:
             self.stats.game_active = True
             self.sb.prep_score()
             self.sb.prep_level()
+            self.sb.prep_ships()
             # Get rid of any remaining aliens and bullets
             self.aliens.empty()
             self.bullets.empty()
@@ -147,6 +148,7 @@ class AlienInvasion:
         if self.stats.ship_left > 0:
             # Decrement ships_lect
             self.stats.ship_left -= 1
+            self.sb.prep_ships()
             # Get rid of any remaining aliens and bullets
             self.aliens.empty()
             self.bullets.empty()
